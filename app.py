4a3cc432f6647f4954b14bf4b568adb98f6ed4e0
rpdentrechales <rpdprocorpo@gmail.com>
2024-08-22 15:28:16 +0000

    Updating app.py

--- app.py
+++ app.py
@@ -1,5 +1,41 @@
 import streamlit as st 
-# Set the app title 
-st.title('My First Streamlit App') 
-# Add a welcome message 
-st.write('Welcome to my Streamlit app!') 
+import altair as alt
+import pandas as pd
+
+# Show the page title and description.
+st.set_page_config(page_title="Pró-Corpo - Dash de Mídia", page_icon="💎")
+st.title("💎 Facebook Ads")
+
+# Load the data from a CSV. We're caching this so it doesn't reload every time the app
+# reruns (e.g. if the user interacts with the widgets).
+@st.cache_data
+def load_data():
+    df = pd.read_csv("/data/fb_data_clean.csv")
+    return df
+
+
+df = load_data()
+
+# Show a multiselect widget with the genres using `st.multiselect`.
+unidades = st.multiselect(
+    "Unidades",
+    df["store"].unique(),
+    default = ['IPIRANGA', 'TUCURUVI', 'MOEMA', 'JARDINS', 'ITAIM']
+)
+
+# Filter the dataframe based on the widget input and reshape it.
+df_filtered = df[df["store"].isin(unidades)]
+df_reshaped = df_filtered.pivot_table(
+    index="month", columns="category", values="Resultados", aggfunc="sum", fill_value=0
+)
+df_reshaped = df_reshaped.sort_values(by="month", ascending=False)
+
+df_reshaped
+
+
+# Display the data as a table using `st.dataframe`.
+st.dataframe(
+    df_reshaped,
+    use_container_width=True,
+    column_config={"year": st.column_config.TextColumn("Year")},
+)
