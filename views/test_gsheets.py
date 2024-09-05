76b9dc0c5874e5bdac0d7677e4ab0da1f45393b4
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-05 15:52:58 +0000

    Updating files

--- views/test_gsheets.py
+++ views/test_gsheets.py
@@ -1,10 +1,15 @@
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
 st.title("Read Google Sheet as DataFrame")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(worksheet="FB - Compilado")
+
+df.drop_duplicates(subset=["Day","Ad Set ID"],inplace=True)
+
+conn.update(data=df,worksheet="FB - Compilado")
 
 st.dataframe(df)
