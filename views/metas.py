3ebb692fe73aa21350ad4b2be7570146aae89ad7
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-19 15:31:48 +0000

    Updating files

--- views/metas.py
+++ views/metas.py
@@ -60,7 +60,7 @@ edited_df = st.data_editor(filtered_metas,
                           )
 
 def upload_changes(df_edited):
-  
+
   df_original = load_dataframe("aux - Configurar metas")
   df_to_upload = pd.concat([df_original,df_edited])
   df_to_upload = df_to_upload.drop_duplicates(subset=["plataforma","month","categoria"],keep="last")
@@ -73,7 +73,7 @@ def upload_changes(df_edited):
     response = "Erro"
   
 
-if st.button("Salvar modificações",on_click=upload_changes,args=(df_metas,edited_df)):
+if st.button("Salvar modificações",on_click=upload_changes,args=(edited_df)):
   if "callback_result" in st.session_state:
     df_metas = st.session_state["callback_result"]
     st.balloons()
