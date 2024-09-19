50987db60a6051b76e22f582e82424fe92e1f03f
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-19 14:57:30 +0000

    Updating files

--- views/metas.py
+++ views/metas.py
@@ -39,7 +39,7 @@ with filtro_2:
 filtered_metas = df_metas.loc[df_metas["month"] == period_filter]
 
 if filtered_metas.shape[0] == 0:
-  filtered_metas = pd.DataFrame(columns=["plafaforma","month","categoria","meta"])
+  filtered_metas = pd.DataFrame(columns=["plataforma","month","categoria","meta"])
   filtered_metas["categoria"] = categorias
   filtered_metas["plataforma"] = plataforma_filter
   filtered_metas["month"] = period_filter
