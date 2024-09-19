d6afc645e7af14d33d907861285464b950827514
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-19 13:56:06 +0000

    Updating files

--- views/metas.py
+++ views/metas.py
@@ -37,7 +37,7 @@ edited_df = st.data_editor(filtered_metas,
                                     format="R$ %.2f",
                                 )}
                           )
-df_metas = pd.concat(edited_df,df_metas)
+df_metas = pd.concat([edited_df,df_metas])
 df_metas.drop_duplicates(subset=["plataforma","month","categoria"],keep="last")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
