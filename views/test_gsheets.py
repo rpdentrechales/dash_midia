fb5d066a91dddc5bd81e8af7db3c2fed9fa48cb0
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-20 14:27:13 +0000

    Updating files

--- views/test_gsheets.py
+++ views/test_gsheets.py
@@ -9,12 +9,14 @@ texto = st.session_state["text"]
 st.write(texto)
 
 if st.button("Change"):
+
   if st.session_state["count"] % 2 == 0:
     st.session_state["text"] = "AQUILO"
-    st.session_state["count"] += 1
+  
   elif st.session_state["count"] % 2 == 1:
     st.session_state["text"] = "Aquilo Outro"
-    st.session_state["count"] += 1
-
+  
+  st.session_state["count"] += 1
 st.write(st.session_state["text"])
+st.write(st.session_state["count"])
 
