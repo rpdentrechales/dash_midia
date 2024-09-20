5eb61c7642318dffa9771f1ecc98bf22a08f4637
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-20 14:34:10 +0000

    Updating files

--- views/test_gsheets.py
+++ views/test_gsheets.py
@@ -1,23 +1,23 @@
 
 import streamlit as st
 
-st.session_state["count"] = 0
-st.session_state["text"] = "ISSO"
+# Use setdefault to initialize session state variables
+st.session_state.setdefault("count", 0)
+st.session_state.setdefault("text", "ISSO")
 
 texto = st.session_state["text"]
 
 st.write(texto)
 
 def change_state():
-  if st.session_state["count"] % 2 == 0:
-    st.session_state["text"] = "AQUILO"
-  
-  elif st.session_state["count"] % 2 == 1:
-    st.session_state["text"] = "Aquilo Outro"
-  
-  st.session_state["count"] += 1
+    if st.session_state["count"] % 2 == 0:
+        st.session_state["text"] = "AQUILO"
+    elif st.session_state["count"] % 2 == 1:
+        st.session_state["text"] = "Aquilo Outro"
+    
+    st.session_state["count"] += 1
 
-st.button("change",on_click = change_state)
+st.button("change", on_click=change_state)
 
 st.write(st.session_state["text"])
 st.write(st.session_state["count"])
