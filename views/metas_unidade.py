a0cc2f59734e361b4a0116979990bb35738e2a97
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-25 15:27:43 +0000

    Updating files

--- views/metas_unidade.py
+++ views/metas_unidade.py
@@ -72,7 +72,8 @@ with display_1:
   
 with display_2:
   soma_metas = pd.DataFrame(edited_df["meta facebook"] + edited_df["meta google"], columns=["total"])
-  soma_metas["Total"] = soma_metas["Total"].fillna(0)
+  soma_metas["total"] = soma_metas["total"].fillna(0)
+
   st.dataframe(soma_metas,
                hide_index = True,
                column_config={
