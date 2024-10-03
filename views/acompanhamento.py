8207457113f022f411b376581a3eff4b02035015
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-03 14:15:52 +0000

    Updating files

--- views/acompanhamento.py
+++ views/acompanhamento.py
@@ -87,9 +87,7 @@ if (store_filter):
   df_filtered = df_sem_cirurgia.loc[df_sem_cirurgia['Unidade'] == store_filter]
   meta_selecionada = df_meta_unidade_mes.loc[df_meta_unidade_mes['unidade'] == store_filter]
 
-meta_facebook = meta_selecionada.loc[0, 'meta facebook']
-
-st.markdown(meta_facebook)
+st.dataframe(meta_selecionada)
 
 metrics_unidade_1,metrics_unidade_2,metrics_unidade_3 = st.columns(3)
 
